78435449ffb1126138a7a305ecde06773e5ed5b0
Akmal Zuhdy Prasetya <54744527+trueazp@users.noreply.github.com>
2019-09-26 22:08:14 +0800

    Fixed code

--- H071191035/TugasPraktikum3/Nomor2.java
+++ H071191035/TugasPraktikum3/Nomor2.java
@@ -8,14 +8,9 @@ class Nomor2 {
         int x = in.nextInt();
         System.out.print("Input y : ");
         int y = in.nextInt();
-        int num = 1;
-        for (int  i = 0; i <= x; i++) {
-            System.out.print(num + " ");
-            for (int j = 0; j <= y; j++) {
-                System.out.println(num + "\n ");
-                num++;
-            }
-        }
+        for (int i = 1; i <= y; i++){
+            System.out.print(i + (i % x == 0 ? "\n" : " "));
+        } 
         in.close();
     }
 }
